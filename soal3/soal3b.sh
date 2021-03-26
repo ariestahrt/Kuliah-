2156e9434115c7526741e6418c8e497972935f90
Ariesta Heart <ariestaheart@gmail.com>
2021-03-27 00:02:55 +0800

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,20 +1,50 @@
-#!/bin/bash 
+#!/bin/bash
 
-currDir=`pwd`
-DATE=$(date '+%d-%m-%Y')
-if [ ! -d $currDir/$DATE ]; then
-	mkdir $currDir/$DATE
+current_date=`date +'%d-%m-%Y'`
+
+echo "[!] Current date : $current_date"
+
+if [[ ! -d "$current_date" ]]; then
+    # File not exist
+    echo "[!] Making directory $current_date"
+    mkdir "$current_date"
+else
+    # File arleady exist
+    echo "[!] Directory $current_date already exist"
 fi
+
+download_nekopoi(){
+    neko_host="https://loremflickr.com"
+    neko_endpoint=`curl -s -i "https://loremflickr.com/320/240/kitten" | grep "location" | awk -F ': ' '{print $2}' | tr -d '\r'`
+    neko_to_download="${neko_host}${neko_endpoint}"
+    neko_filename=`echo "$neko_endpoint" | awk -F '/' '{print $4}'`
+
+    echo "Download : $neko_to_download"
+    wget $neko_to_download -P "$current_date"
+
+    echo "Download : $neko_to_download">>"${current_date}/Foto.log"
+
+    if [[ $(cat "${current_date}/Foto.log" | grep -c "${neko_to_download}") -gt 1 ]]; then
+        # Kalau kucingnya udah kedownload, maka hapus lagi kucingnya
+        rm "$current_date/$neko_filename"
+    else
+        # Kalau kucingnya belum kedownload, maka tinggal ganti nama aja
+        neko_number=`ls $current_date | grep -c "Koleksi"`
+        neko_number=`echo "$neko_number + 1" | tr -d $'\r' | bc -l`
+
+        if [ 1 -eq "$(echo "${neko_number} < 10" | bc)" ]
+        then
+            neko_number="0${neko_number}"
+        fi
+        
+        neko_save_file_name="Koleksi_${neko_number}.jpg"
+
+        # echo $neko_save_file_name
+        mv "$current_date/$neko_filename" "$current_date/$neko_save_file_name"
+    fi
+}
 
 for ((num=1; num<=23; num=num+1))
 do
-    if [ $num -lt 10 ]
-    then
-        wget https://loremflickr.com/320/240/kitten -O $currDir/Koleksi_0$num.jpg -a Foto.log
-    else
-        wget https://loremflickr.com/320/240/kitten -O $currDir/Koleksi_$num.jpg -a Foto.log
-    fi
+    download_nekopoi
 done
-
-find $currDir -type f -name "*.jpg" -exec mv {} $currDir/$DATE \;
-find $currDir -type f -name "*.log" -exec mv {} $currDir/$DATE \;
