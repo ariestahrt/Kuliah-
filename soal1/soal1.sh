9ffc6a8f72e1e9662b8893b98482348719349ed0
Ariesta Heart <ariestaheart@gmail.com>
2021-03-27 01:22:14 +0800

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,100 +1,20 @@
 #!/bin/bash
+IFS=$'\n'
 
-'''
-Praktikum Modul 1
-Sistem Operasi 2021
+echo "Error,Count">error_message.csv
+echo "Username,INFO,ERROR">user_statistic.csv
+# D
 
-Kelompok C04
+for line in $(cat syslog.log | grep "ERROR" | grep -oP "ERROR\s\K.*(?=\s\()" | sort | uniq)
+do
+    total=$(cat syslog.log | grep -c "$line")
+    echo $line,$total
+done | sort -rnk 2 -t','>>error_message.csv
 
-I Kadek Agus Ariesta Putra  05111940000105
-Muhammad Arif Faizin        05111940000060
-Ahmad Lamaul Farid          05111940000134
-
-SOAL 1
-'''
-
-# Arraylist untuk menyimpan data
-error_msg_list=()
-error_msg_count=()
-
-user_list=()
-user_list_error_count=()
-user_list_info_count=()
-
-while read line; do
-    username=$(echo "$line" | grep -o -P '(?<=\().*(?=\))')
-    is_info=$(echo $line | grep -c "INFO")
-
-    jenis="ERROR"
-    if [[ is_info -gt 0 ]]; then
-        jenis="INFO"
-    fi
-
-    pesan=$(echo $line | grep -oP "${jenis}\s*\K.*(?= \(${username}\))")
-
-    # Check dulu usernya udah ada apa belum di array user_list
-    index_user=0
-    user_sudah_ada=0
-    for item in "${user_list[@]}"; do
-        if [[ $username == $item ]]; then
-            user_sudah_ada=1
-            break
-        fi
-        index_user=$(($index_user+1))
-    done
-
-    # Kalau belum ada masukin ke array
-    if [[ $user_sudah_ada == 0 ]]; then
-        user_list+=("$username")
-        user_list_error_count[index_user]=0
-        user_list_info_count[index_user]=0
-    fi
-    
-    if [[ $jenis == "ERROR" ]]; then
-        # Simpan total error ke user_list_error_count
-        user_list_error_count[index_user]=$((${user_list_error_count[$index_user]}+1))
-
-        index=0
-        sudah_ada=0
-        # Menghitung semua error dan pesannya muncul berapa kali
-        for item in "${error_msg_list[@]}"; do
-            if [[ $pesan == $item ]]; then
-                sudah_ada=1
-                break
-            fi
-            index=$(($index+1))
-        done
-        
-        if [[ $sudah_ada == 1 ]]; then
-            # Sudah ada
-            error_msg_count[index]=$((${error_msg_count[$index]}+1))
-        else
-            # Belum Ada
-            error_msg_list+=("$pesan")
-            error_msg_count[index]=$((${error_msg_count[$index]}+1))
-        fi
-    else
-        # echo "TEST"
-        user_list_info_count[index_user]=$((${user_list_info_count[$index_user]}+1))
-    fi
-
-done <syslog.log
-
-# B Menampilkan semua error dan pesannya muncul berapa kali
-
-echo "Error,Count" > error_message.csv
-
-for ((i = 0; i < ${#error_msg_list[@]}; i=i+1)) do
-    echo "${error_msg_list[$i]},${error_msg_count[$i]}" >> error_message.csv
-    echo -n "${error_msg_list[$i]} : "
-    echo "${error_msg_count[$i]}"
-done
-
-# C Menampilkan total error dan info setiap user
-
-echo "Username,INFO,ERROR" > user_statistic.csv
-
-for ((i = 0; i < ${#user_list[@]}; i=i+1)) do
-    echo "${user_list[$i]},${user_list_info_count[$i]},${user_list_error_count[$i]}" >> user_statistic.csv
-    echo "username : ${user_list[$i]} | INFO : ${user_list_info_count[$i]} | ERROR : ${user_list_error_count[$i]}"
-done
+# E
+for line in $(cat syslog.log | grep -o '(.*)' | tr -d '(' | tr -d ')' | sort | uniq)
+do
+    error=$(cat syslog.log | grep "$line" | grep -c "ERROR")
+    info=$(cat syslog.log | grep "$line" | grep -c "INFO")
+    echo $line,$error,$info
+done >> user_statistic.csv
