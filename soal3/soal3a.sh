db5473bf32d35ec8b5af85c1062ad8dfaa2c4b8c
Ariesta Heart <ariestaheart@gmail.com>
2021-03-27 00:02:36 +0800

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
+
+download_nekopoi(){
+    neko_host="https://loremflickr.com"
+    neko_endpoint=`curl -s -i "https://loremflickr.com/320/240/kitten" | grep "location" | awk -F ': ' '{print $2}' | tr -d '\r'`
+    neko_to_download="${neko_host}${neko_endpoint}"
+    neko_filename=`echo "$neko_endpoint" | awk -F '/' '{print $4}'`
+
+    echo "Download : $neko_to_download"
+    wget $neko_to_download
+
+    echo "Download : $neko_to_download">>Foto.log
+
+    if [[ $(cat Foto.log | grep -c "${neko_to_download}") -gt 1 ]]; then
+        # Kalau kucingnya udah kedownload, maka hapus lagi kucingnya
+        rm $neko_filename
+    else
+        # Kalau kucingnya belum kedownload, maka tinggal ganti nama aja
+        neko_number=`ls | grep -c "Koleksi"`
+        neko_number=`echo "$neko_number + 1" | tr -d $'\r' | bc -l`
+
+        if [ 1 -eq "$(echo "${neko_number} < 10" | bc)" ]
+        then
+            neko_number="0${neko_number}"
+        fi
+        
+        neko_save_file_name="Koleksi_${neko_number}.jpg"
+
+        # echo $neko_save_file_name
+        mv $neko_filename "$neko_save_file_name"
+    fi
+}
+
+>Foto.log
 for ((num=1; num<=23; num=num+1))
 do
-    if [ $num -lt 10 ]
-    then
-        wget https://loremflickr.com/320/240/kitten -O Koleksi_0$num.jpg -a Foto.log
-    else
-        wget https://loremflickr.com/320/240/kitten -O Koleksi_$num.jpg -a Foto.log
-    fi
+    download_nekopoi
 done
