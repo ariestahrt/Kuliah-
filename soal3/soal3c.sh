e4b4e16cc811b87b89e9d95206c8a607d27b331e
Ariesta Heart <ariestaheart@gmail.com>
2021-03-27 21:14:07 +0800

    Update soal3c.sh
    Simplyfied

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -5,15 +5,19 @@ yesterday_date=`date -d "yesterday" +'%d-%m-%Y'`
 echo "[!] Current date : $current_date"
 echo "[!] Yesterday date : $yesterday_date"
 
-# Fungsi donlot neko
+# Fungsi donlot
 
-download_nekopoi(){
+download_foto(){
+    animal=$1
+    hewan=$2
+    mkdir "${hewan}_$current_date"
+
     host="https://loremflickr.com"
-    endpoint=`curl -s -i "https://loremflickr.com/320/240/kitten" | grep "location" | awk -F ': ' '{print $2}' | tr -d '\r'`
+    endpoint=`curl -s -i "https://loremflickr.com/320/240/${animal}" | grep "location" | awk -F ': ' '{print $2}' | tr -d '\r'`
     to_download="${host}${endpoint}"
     filename=`echo "$endpoint" | awk -F '/' '{print $4}'`
 
-    path_save="Kucing_$current_date"
+    path_save="${hewan}_$current_date"
 
     echo "Download : $to_download"
     wget $to_download -P "$path_save"
@@ -21,10 +25,10 @@ download_nekopoi(){
     echo "Download : $to_download">>"$path_save/Foto.log"
 
     if [[ $(cat "$path_save/Foto.log" | grep -c "${to_download}") -gt 1 ]]; then
-        # Kalau kucingnya udah kedownload, maka hapus lagi kucingnya
+        # Kalau hewannya udah kedownload, maka hapus lagi hewannya
         rm "$path_save/$filename"
     else
-        # Kalau kucingnya belum kedownload, maka tinggal ganti nama aja
+        # Kalau hewannya belum kedownload, maka tinggal ganti nama aja
         file_number=`ls $path_save | grep -c "Koleksi"`
         file_number=`echo "$file_number + 1" | tr -d $'\r' | bc -l`
 
@@ -40,71 +44,20 @@ download_nekopoi(){
     fi
 }
 
-# Fungsi donlot kelinci
-download_kelinci(){
-    host="https://loremflickr.com"
-    endpoint=`curl -s -i "https://loremflickr.com/320/240/bunny" | grep "location" | awk -F ': ' '{print $2}' | tr -d '\r'`
-    to_download="${host}${endpoint}"
-    filename=`echo "$endpoint" | awk -F '/' '{print $4}'`
-
-    path_save="Kelinci_$current_date"
-
-    echo "Download : $to_download"
-    wget $to_download -P "$path_save"
-
-    echo "Download : $to_download">>"Kelinci_${current_date}/Foto.log"
-
-    if [[ $(cat "$path_save/Foto.log" | grep -c "${to_download}") -gt 1 ]]; then
-        # Kalau kelincinya udah kedownload, maka hapus lagi kucingnya
-        rm "$path_save/$filename"
-    else
-        # Kalau kelincinya belum kedownload, maka tinggal ganti nama aja
-        file_number=`ls $path_save | grep -c "Koleksi"`
-        file_number=`echo "$file_number + 1" | tr -d $'\r' | bc -l`
-
-        if [ 1 -eq "$(echo "${file_number} < 10" | bc)" ]
-        then
-            file_number="0${file_number}"
-        fi
-        
-        save_file_name="Koleksi_${file_number}.jpg"
-
-        mv "$path_save/$filename" "$path_save/$save_file_name"
-    fi
-}
-
 # Get yesterday file
 
 isyesterday_kucing=`ls -l | grep "$yesterday_date" | grep -c "Kucing"`
-echo "Is Yesterday Kucing" $isyesterday_kucing
 
 if [[ $isyesterday_kucing -eq 0 ]]; then
-    if [[ ! -d "Kucing_$current_date" ]]; then
-        # File not exist
-        echo "[!] Making directory Kucing_$current_date"
-        mkdir "Kucing_$current_date"
-    else
-        # File arleady exist
-        echo "[!] Directory Kucing_$current_date already exist"
-    fi
     # Then download neko
     for ((num=1; num<=23; num=num+1))
     do
-        download_nekopoi
+        download_foto "kitten" "Kucing"
     done
 else
-    if [[ ! -d "Kelinci_$current_date" ]]; then
-        # File not exist
-        echo "[!] Making directory Kelinci_$current_date"
-        mkdir "Kelinci_$current_date"
-    else
-        # File arleady exist
-        echo "[!] Directory Kelinci_$current_date already exist"
-    fi
-
-    # Then download Kelinci
+    # Then download neko
     for ((num=1; num<=23; num=num+1))
     do
-        download_kelinci
+        download_foto "bunny" "Kelinci"
     done
 fi
