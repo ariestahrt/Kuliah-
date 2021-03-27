8699403873eb594f0abeb854df9a07d7cebfe1bb
Ariesta Heart <ariestaheart@gmail.com>
2021-03-27 22:10:59 +0800

    Update soal3d.sh

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Unzip : unzip -P {PWD} Koleksi.zip
+# Unzip : password=`date +'%m%d%Y'` && unzip -P $password Koleksi.zip && rm Koleksi.zip
 
 password=`date +'%m%d%Y'`
 
 for d in */ ; do
-    echo "$d"
     zip -r -P "$password" Koleksi.zip . -i "$d*"
     rm -rf "$d"
 done
